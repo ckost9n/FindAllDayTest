5619fb6f68fcfd64c14e34c05ba3f39653eb2e24
ckost9n <ckost9n@gmail.com>
2021-08-14 21:44:19 +0300

    finish app

--- FindAllDayTest/ViewController.swift
+++ FindAllDayTest/ViewController.swift
@@ -17,6 +17,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dayTF.text, let month = mounthTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        var dateComponents = DateComponents()
+        
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
+        
+        resultLabel.text = capitalizedWeekday
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
     }
 }
 
